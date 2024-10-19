7aec8d04c234ed46bc7f859a92ab04e58ee025ff
unknown <shreejanofficial2007@gmail.com>
2024-10-19 21:17:39 +0530

    Final commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from keras.models import load_model # type: ignore
+from keras.models import load_model 
 import streamlit as st
 import matplotlib.pyplot as plt
 
